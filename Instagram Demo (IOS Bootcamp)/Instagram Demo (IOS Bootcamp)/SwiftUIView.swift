3f10c1b101760a7591506c11d0e616b45ee819c2
Sherry Chen <shchen@ad.unc.edu>
2024-09-29 18:47:34 -0400

    Upload with action button

--- Instagram Demo (IOS Bootcamp)/Instagram Demo (IOS Bootcamp)/SwiftUIView.swift
+++ Instagram Demo (IOS Bootcamp)/Instagram Demo (IOS Bootcamp)/SwiftUIView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    @State private var like: Bool = false
+    @State private var save: Bool = false
+    @State private var likeCount: Int = 0
     var body: some View {
         VStack(alignment: .leading, spacing: 8){
             HStack{
@@ -21,7 +24,7 @@ struct SwiftUIView: View {
                     .bold()
                 Spacer()
                 Image(systemName: "ellipsis")
-                        
+                
             }
             .padding(.horizontal, 8)
             Image(.bunny2)
@@ -29,29 +32,51 @@ struct SwiftUIView: View {
                 .scaledToFit()
             
             HStack(spacing: 16){
-                Image(systemName:"heart")
+                Button(action: {
+                    if self.like == true && likeCount > 0 {
+                        likeCount -= 1
+                    } else {
+                        likeCount += 1
+                    }
+                    self.like.toggle()
+                }) {
+                    if self.like == true {
+                        Image(systemName:"heart.fill")
+                            .foregroundStyle(.red)
+                    } else {
+
+                            Image(systemName:"heart")
+                                .foregroundStyle(.black)
+                        }
+                }
+                Text("\(likeCount) likes")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .padding(.horizontal, -10)
                 Image(systemName:"message")
                 Image(systemName:"paperplane")
                 Spacer()
-                Image(systemName:"bookmark")
+                Button(action: {self.save.toggle()}) {
+                    Image(systemName: self.save == true ? "bookmark.fill" : "bookmark")
+                        .foregroundStyle(.black)
+                        .font(.title2)
+                }
+                    
+                
             }
-            .font(.title2)
-                Text("100 likes")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
-                    .padding(.horizontal, 8)
+            HStack{
                 Text("**Cherry** Happy Bunny")
                     .font(.footnote)
                     .padding(.horizontal, 8)
+            }
                 Text("1 hour ago")
                     .font(.caption)
                     .foregroundStyle(.secondary)
                     .padding(.horizontal, 8)
-            
+                
         }
     }
 }
-
 #Preview {
     SwiftUIView()
 }
